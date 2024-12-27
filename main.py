805b6e509cc3eb9154959357bbfd076738bafa36
MilkShake <andr3s33@gmx.com>
2024-12-27 16:18:17 -0600

    2nd

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import win32api,win32con
 
 TIME_FACTOR_DIV = 1
 
-GAMESTATES = ["outside", "inbattle", "afterbattle", "train"]
+CAN_TRAIN = False
+ELEMENTS_ARRAY = []
+ELEMENTS_ARRAY_ACTUAL = []
+GAMESTATES = ["outside", "inbattle", "afterbattle"]
 GAMESTATE = GAMESTATES[0]
 
 entranceToTown = (213,507)
@@ -18,36 +21,92 @@ def print_gamestate():
    print(f'Game state: {GAMESTATE}')
 
 def check_gamestate():
-   global GAMESTATE
+    global GAMESTATE   
+    #pyautogui.useImageNotFoundException()  outdated
 
-   if(pyautogui.locateOnScreen("assets/png/ui01.png", confidence=0.65) is not None):
-      GAMESTATE = GAMESTATES[0]
-      print_gamestate()
+    try:
+        if (pyautogui.locateOnScreen("assets/png/ui01.png", confidence=0.65) is not None):
+            GAMESTATE = GAMESTATES[0]
+            print_gamestate()
+
+    except pyautogui.ImageNotFoundException:
+        pass
+
+    try:
+        if (pyautogui.locateOnScreen("assets/png/ui00.png", confidence=0.5) is not None):
+            GAMESTATE = GAMESTATES[1]
+            print_gamestate()
+
+    except pyautogui.ImageNotFoundException:
+        pass
+
+    try:
+        if (pyautogui.locateOnScreen("assets/png/ui02.png", confidence=0.5) is not None):
+            GAMESTATE = GAMESTATES[2]
+            print_gamestate()
+
+    except pyautogui.ImageNotFoundException:
+        pass
+   
       
-   elif (pyautogui.locateOnScreen("assets/png/ui00.png", confidence=0.5) is not None):
-      GAMESTATE = GAMESTATES[1]
-      print_gamestate()
-
-   else:
-      print_gamestate()
   
   
 
 def find_bush():
+    global GAMESTATE
 
     if(GAMESTATE==GAMESTATES[0]):
-        loc = pyautogui.locateCenterOnScreen("assets/png/element00.png", confidence=0.7)
-        pyautogui.moveTo(loc)
-        pyautogui.click()
-        time.sleep(1)
+        try:
+            loc = pyautogui.locateCenterOnScreen("assets/png/element00.png", confidence=0.7)
+            pyautogui.moveTo(loc)
+            pyautogui.click()
+            GAMESTATE = GAMESTATES[1]
+            time.sleep(1.5)
+
+        except pyautogui.ImageNotFoundException:
+            pass
+        
+        
 
 def battle():
    if(GAMESTATE==GAMESTATES[1]):
-      loc = pyautogui.locateCenterOnScreen("assets/png/ui00.png", confidence=0.55)
-      pyautogui.moveTo(loc)
-      pyautogui.click()
-      time.sleep(1)
+      
+      try:
+          loc = pyautogui.locateCenterOnScreen("assets/png/ui00.png", confidence=0.45)
+          pyautogui.moveTo(loc)
+          pyautogui.click()
+          time.sleep(1)
 
+      except pyautogui.ImageNotFoundException:
+        pass
+
+
+def after_battle():
+    global CAN_TRAIN
+
+    if(GAMESTATE==GAMESTATES[2]):            
+      try:
+          
+          try:                            
+              if(pyautogui.locateCenterOnScreen("assets/png/ui05.png", confidence=0.5)):
+                                                      
+                  CAN_TRAIN = True
+                  print("Can Train now.")
+
+          except pyautogui.ImageNotFoundException:
+              pass
+          
+          loc = pyautogui.locateCenterOnScreen("assets/png/ui03.png", confidence=0.3)
+            
+          pyautogui.moveTo(loc)
+          pyautogui.click()
+          time.sleep(1)
+
+      except pyautogui.ImageNotFoundException:
+        pass
+
+        
+      
     
 
 if __name__ == "__main__":
@@ -62,8 +121,11 @@ if __name__ == "__main__":
     check_gamestate()
     find_bush()
     battle()
+    after_battle()
+
 
     currMx,currMy = pyautogui.position()
     print(currMx,currMy)
-
     print(GAMESTATE)
+
+    time.sleep(0.1)
